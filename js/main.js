d74d9f676b338af737699de9d5f55425881458e5
Александр Варнаев <a.varnaev@inter.local>
2020-10-19 17:06:56 +0300

    to ghpages

--- js/main.js
+++ js/main.js
@@ -1,85 +1,48 @@
-(function () {
-    "use strict";
-
-    function initialize() {
-        var mapOptions = {
-            zoom: 14,
-            center: new google.maps.LatLng(23.7893837, 90.38596079999999),
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-
-        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(23.7893837, 90.38596079999999),
-        });
-
-        marker.setMap(map);
-        var infowindow = new google.maps.InfoWindow({
-            content: "Hello World!"
-        });
-
-        infowindow.open(map, marker);
-
-        //add overlay
-        map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(document.getElementById('map-overlay'));
+ymaps.ready(init);
+var placemarks = [
+    {
+        latitude: 56.46900168645355,
+        longitude: 84.9647499562564,
+        hintContent: '<div class="map__hint">ROCK GYM</div>',
+        balloonContent: [
+            '<div class="map__balloon">',
+            'Работаем для Вас с 9 до 24 ежедневно',
+            '</div>'
+        ]
     }
+]
+geoObjects = [];
 
-    google.maps.event.addDomListener(window, 'load', initialize);
-   // navigator.geolocation.getCurrentPosition(initialize);
-    jQuery(document).ready(function ($) {
-
-
-        $(".embed-responsive iframe").addClass("embed-responsive-item");
-        $(".carousel-inner .item:first-child").addClass("active");
-
-        $('[data-toggle="tooltip"]').tooltip();
-
-        $('.gallery-btn a').click(function () {
-
-            $(".gallery-btn a").removeClass("active");
-            $(this).addClass("active");
-
-            var selector = $(this).attr('data-filter');
-            $(".gellery-item").isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
-       // jQeury smooth scroll
-        $("li.smooth-scroll a").bind("click", function(event){
-            var $anchor = $(this);
-            var headerH = "50";
-            $("html, body")
-                .stop()
-                .animate({
-                scrollTop: $($anchor.attr("href"))
-                    .offset()
-                    .top - headerH + "px"
-            }, 1200, "easeOutCirc");
-
-            event.preventDefault();
-
-        });
-        //jquery scroll spy
-        $("body").scrollspy({
-            target: ".navbar-collapse",
-            offset: 95
-        });
-        new WOW().init();
-        //Pretty Photo
-        $("a[data-gallery^='prettyPhoto']").prettyPhoto({
-            social_tools: false
-        });
+function init() {
+    var map = new ymaps.Map('map', {
+        center: [56.46900168645355, 84.9647499562564],
+        zoom: 18,
+        controls: ['zoomControl'],
+        behaviors: ['drag']
     });
 
+    for (var i = 0; i < placemarks.length; i++) {
+        geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude],
+            {
+                hintContent: placemarks[i].hintContent,
+                balloonContent: placemarks[i].balloonContent.join('')
+            },
+            {
+                iconLayout: 'default#image',
+                iconImageHref: './img/logo-red-black.png',
+                iconImageSize: [70, 70],
+            });
+    }
 
-    jQuery(window).load(function () {});
+    var clusterer = new ymaps.Clusterer();
+    clusterer.createCluster = function (center, geoObjects) {
+        var clusterPlacemark = ymaps.Clusterer.prototype.createCluster.call(this, center, geoObjects);
 
+        clusterPlacemark.properties.set('hintContent', 'Нажмите чтобы увидеть больше');
 
-}(jQuery));
+        return clusterPlacemark;
+    };
+
+    map.geoObjects.add(clusterer);
+    clusterer.add(geoObjects);
+}
